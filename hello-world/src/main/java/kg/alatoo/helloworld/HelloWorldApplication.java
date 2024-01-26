e42f54cf0c85a7a8c44c05e1900585a8f6fb6f4f
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-01-26 11:31:49 +0600

    26.01.24 Hello world and entities

---
+++ hello-world/src/main/java/kg/alatoo/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+}
---
+++ hello-world/src/main/java/kg/alatoo/helloworld/controllers/ApiController.java
@@ -0,0 +1,19 @@
+package kg.alatoo.helloworld.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping
+    public String hello() {
+        return "<h1>Hello Spring!</h1><p>Welcome to our spring course!</p>";
+    }
+
+    @GetMapping("welcome")
+    public String welcomeAibika() {
+        return "Welcome, Aibika";
+    }
+}
