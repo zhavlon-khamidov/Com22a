3bf7c4d06f81abd7c750a0a5bbc4669ce0c4031d
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-03-19 11:31:18 +0600

    Introduction to spring security

---
+++ library-app/src/main/java/kg/alatoo/libraryapp/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package kg.alatoo.libraryapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"));
+
+        http.authorizeHttpRequests(req -> req.requestMatchers(HttpMethod.POST,"/api/v1/book/**").hasRole("ADMIN")
+            .anyRequest().authenticated());
+
+        http.httpBasic(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
