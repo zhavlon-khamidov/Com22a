002eef4cad80874fbf367c19711a1aef17d3a041
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-04-01 10:46:18 +0600

    Review spring security, introduces DelegatingPasswordEncoder

--- library-app/src/main/java/kg/alatoo/libraryapp/config/SecurityConfig.java
+++ library-app/src/main/java/kg/alatoo/libraryapp/config/SecurityConfig.java
@@ -5,21 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"));
 
-        http.authorizeHttpRequests(req -> req.requestMatchers(HttpMethod.POST,"/api/v1/book/**").hasRole("ADMIN")
-            .anyRequest().authenticated());
+        http.authorizeHttpRequests(
+                req -> req
+//                        .requestMatchers(HttpMethod.GET,"/api/v1/book").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/book/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+        );
 
         http.httpBasic(Customizer.withDefaults());
         http.formLogin(Customizer.withDefaults());
@@ -27,17 +35,28 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
     @Bean
     public UserDetailsService userDetailsService() {
+        String userPassword = passwordEncoder().encode("user");
+        String adminPassword = passwordEncoder().encode("admin");
+
+        System.out.println("userPassword = " + userPassword);
+        System.out.println("adminPassword = " + adminPassword);
+
         UserDetails user = User.builder()
                 .username("user")
-                .password("{noop}user")
+                .password(userPassword)
                 .roles("USER")
                 .build();
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("{noop}admin")
+                .password(adminPassword)
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
--- library-app/src/main/java/kg/alatoo/libraryapp/services/UserDetailsServiceImpl.java
+++ library-app/src/main/java/kg/alatoo/libraryapp/services/UserDetailsServiceImpl.java
@@ -3,9 +3,8 @@ package kg.alatoo.libraryapp.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
